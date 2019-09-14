b3aea7c148850679cff75d735ddb5ca2ddfd55e4
Jannah June Nieves <jannahjune@gmail.com>
2019-09-13 22:26:26 -0700

    low inventory code corrected and added

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,7 +13,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 	if(err)throw err;
 	console.log("connected as id" + connection.threadId);
+	// displayList();
 });
+
+
+function inquirerPrompt(){
+	inquirer.prompt([{
+		name:"action",
+		type: "list",
+		message: "Choose from the provided options to manage the current inventory:",
+		choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add A New Product", "Remove An Existing Product", "Exit"]
+	}]).then(function(answers){
+		switch(answers.action){
+			case "View Products for Sale":
+				displayList();
+				break;
+			case "View Low Inventory":
+				lowInventory();
+				break;
+			case "Add to Inventory":
+				addInventoryReq();
+				break;
+			case "Add A New Product":
+				addItemRequest();
+				break;
+			case "Remove An Existing Product":
+				removeItemRequest();
+				break;	
+			case "Exit":
+				exit();	
+				break;
+		}
+	});
+};
 
 function displayList(){
     var query = "SELECT * FROM products";
@@ -29,32 +61,24 @@ function displayList(){
 				);
 		}
 		console.log(displayTable.toString());
-		inquirerUpdate();
+		inquirerPrompt();
 	});
 };
 
-function inquirerUpdate(){
-	inquirer.prompt([{
-		name:"action",
-		type: "list",
-		message: "Choose from the provided options to manage the current inventory:",
-		choices: ["Restock Inventory", "Add A New Product", "Remove An Existing Product"]
-	}]).then(function(answers){
-		switch(answers.action){
-			case "Restock Inventory":
-				restockRequest();
-				break;
-			case "Add A New Product":
-				addRequest();
-				break;
-			case "Remove An Existing Product":
-				removeRequest();
-				break;		
+function lowInventory(){
+	var query = "SELECT * FROM products WHERE stock_quantity<5";
+	connection.query(query, function(err,res) {
+		if (err) throw err;
+		console.log("The resulting items have low inventory.");
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].product_name + " || Item ID: " + res[i].item_id + " || There are " + res[i].stock_quantity + " in stock.\n");
 		}
+		inquirerPrompt();
 	});
 };
 
-function restockRequest(){
+
+function addInventoryReq(){
 	inquirer.prompt([
 	{
 		name: "ID",
@@ -69,11 +93,11 @@ function restockRequest(){
 	]).then(function(answers){
         var productID = answers.ID;
 		var qtyadded = answers.Quantity;
-		restockInventory(productID, qtyadded);
+		addInventory(productID, qtyadded);
 	});
 };
 
-function restockInventory(productID, qtyadded){
+function addInventory(productID, qtyadded){
 	connection.query("SELECT * FROM Products WHERE item_id = " + productID, function(err,res){
 		if(err){console.log(err)};
         // connection.query('UPDATE Products SET stock_quantity = stock_quantity + ' + qtyadded + 'WHERE item_id =' + productID);
@@ -85,7 +109,7 @@ function restockInventory(productID, qtyadded){
 	});
 };
 
-function addRequest(){
+function addItemRequest(){
 	inquirer.prompt([
 	{
 		name: "ID",
@@ -101,7 +125,7 @@ function addRequest(){
 	{
 		name: "Department",
 		type: "input",
-		message: "What department does your product fall in?"
+		message: "What department does your product belong to?"
 	},
 	{
 		name: "Price",
@@ -118,8 +142,8 @@ function addRequest(){
 		var id = answers.ID;
 		var name = answers.Name;
 		var department = answers.Department;
-		var price = answers.Price;
-		var quantity = answers.Quantity;
+		var price = parseFloat(answers.Price).toFixed(2);
+		var quantity = parseInt(answers.Quantity);
 		addItem(id,name,department,price,quantity); 
 	});
   };
@@ -129,20 +153,29 @@ function addItem(id,name,department,price,quantity){
   	displayList();
 };
 
-function removeRequest(){
+function removeItemRequest(){
   	inquirer.prompt([{
   		name: "ID",
   		type: "input",
   		message: "What is the item id of the item you would like to remove?"
   	}]).then(function(answer){
   		var id = answer.ID;
-  		removeInventory(id); 
+  		removeItem(id); 
   	});
 };
 
-function removeInventory(id){
+function removeItem(id){
     connection.query("DELETE FROM Products WHERE item_id = " + id);
   	displayList();
 };
 
+
+function exit() {
+	console.log("\nCome back soon.");
+	connection.end();
+}
+
+
 displayList();
+
+
